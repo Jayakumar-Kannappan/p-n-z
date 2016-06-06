80761c213556046e51530523598528647914753a
Jayakumar-Kannappan <jayakumar96k@gmail.com>
2016-06-06 23:30:40 +0530

    Update and rename README.md to sample.c

---
+++ sample.c
@@ -0,0 +1,19 @@
+# p-n-z
+#include<stdio.h>
+main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+ {
+ printf("positive");
+}
+  else if(n<0)
+   {
+  printf("negative");
+}
+    else
+    {
+  printf("zero");
+}
+}
